87c8b3a534c4acffb43b902fc32d61c63a9f4b5c
Frankie <frankie710@gmail.com>
2020-04-26 21:28:07 -0400

    covid19_healthdata

--- web_scraping/covid19_healthdata_scraper.py
+++ web_scraping/covid19_healthdata_scraper.py
@@ -3,28 +3,39 @@
 from selenium import webdriver
 import time
 
-covid19_healthdate_bystate = []
+covid19_healthdate_dates_bystate = []
+covid19_healthdate_resources_bystate = []
 
 def covid19_healthdata_scrape(state):
-    state = state.lower().replace(' ', '-')
-    url = f"https://covid19.healthdata.org/united-states-of-america/{state}"
+    formatedstate = state.lower().replace(' ', '-')
+    url = f"https://covid19.healthdata.org/united-states-of-america/{formatedstate}"
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(5)
     dates_div = driver.find_elements_by_xpath("/html/body/div/div/main/div[3]/div[1]/div[2]")
-    divs_text = [x.text for x in dates_div]
-    divs_text_split = divs_text[0].split('\n')
-    #print(divs_text_split)
+    dates_divs_text = [x.text for x in dates_div]
+    dates_text_split = dates_divs_text[0].split('\n')
+    resources_div = driver.find_elements_by_xpath("/html/body/div/div/main/div[3]/div[3]/div[2]/div/div[2]/div[2]")
+    resources_div_text = [x.text for x in resources_div]
+    resources_div_split = resources_div_text[0].split('\n')
     driver.close()
-    scraped_data = {
+    dates_data = {
     'state': state,
-    'state_data': {
-        'Mass Gathering Restriction': divs_text_split[1],
-        'Initial Business Closure': divs_text_split[3],
-        'Educational Facilities Closure': divs_text_split[5],
-        'Non-Essential Services Closure': divs_text_split[7],
-        'Stay at Home Order': divs_text_split[9],
-        'Travel Severely Limited': divs_text_split[11]
-        }
+    'Mass Gathering Restriction': dates_text_split[1],
+    'Initual Business Closure': dates_text_split[3],
+    'Educational Facilities Closure': dates_text_split[5],
+    'Non-Essential Services Closure': dates_text_split[7],
+    'Stay at Home Order': dates_text_split[9],
+    'Travel severely limited': dates_text_split[11]}
+    resources_data = {
+    'Hospital Beds Needed': int(resources_div_split[1].replace('beds','').replace(',','')),
+    'Hospital Beds Available': int(resources_div_split[3].replace('beds','').replace(',','')),
+    'Hospital Beds Shortage': int(resources_div_split[5].replace('beds','').replace(',','')),
+    'ICU Beds Needed': int(resources_div_split[7].replace('beds','').replace(',','')),
+    'ICU Beds Available': int(resources_div_split[9].replace('beds','').replace(',','')),
+    'ICU Beds Shortage': int(resources_div_split[11].replace('beds','').replace(',','')),
+    'Ventilators Needed': int(resources_div_split[13].replace('ventilators','').replace(',',''))
     }
-    covid19_healthdate_bystate.append(scraped_data)
+    covid19_healthdate_dates_bystate.append(dates_data)
+    covid19_healthdate_resources_bystate.append(resources_data)
+    
